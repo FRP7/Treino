aab782f563622d9199bc9efd5661eda3a11f044d
FranciscoPires <frelvaspires@gmail.com>
2020-10-07 14:19:17 +0100

    comments added in the login class

--- Teste/Login.cs
+++ Teste/Login.cs
@@ -10,39 +10,55 @@ namespace Teste
         static Register registerinstance = new Register(); //aceder à classe Register
         static string firstInput; //guardar o primeiro input do user
 
-
-        public void LoginPage() {
-            Start();
+        //começa aqui
+        public void LoginPage() { 
+            Start(); //chama o método start
         }
 
+        //aqui é a primeira página do login
         private void Start() {
+            //start
             Console.WriteLine("Página de Login \n");
             Console.WriteLine("Deseja fazer login ou voltar para a página inicial ou registar? \n");
             Console.WriteLine("1 - Login \n");
             Console.WriteLine("2 - Pagina inicial \n");
             Console.WriteLine("3 - Registar \n");
+            //
+            //input do user
             firstInput = Console.ReadLine();
             Console.WriteLine(firstInput);
             FirstChoice();
+            //
         }
 
+        //método para verificar o input do start
         private void FirstChoice() {
+
+            //enviar o user para o login mesmo
             if(firstInput == "1") {
                 Console.WriteLine("Vai começar o login \n");
                 LoginInput();
             }
+
+            //enviar o user de volta para o index
             else if(firstInput == "2") {
                 indexinstance.Return();
 
-            } else if (firstInput == "3") {
+            }
+
+            //enviar o user para o register
+            else if (firstInput == "3") {
                 registerinstance.RegisterPage();
             }
+
+            //chamar o método start de novo em caso de erro(para parecer um restart)
             else {
                 Console.WriteLine("What? Tenta de novo \n");
                 Start();
             }
         }
 
+        /// método onde vai pedir o input do user
         private void LoginInput() {
             Console.WriteLine("Aqui vais meter as credenciais \n");
         }
